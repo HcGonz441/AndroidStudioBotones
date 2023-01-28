a2e6bbcdab94b0f98881c9d886a29180b88155d4
HcGonz441 <hector.gonzalez@iest.edu.mx>
2023-01-27 19:03:38 -0600

    Third Commit

--- app/src/main/java/com/example/botones/MainActivity.kt
+++ app/src/main/java/com/example/botones/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.botones
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -8,16 +9,28 @@ import android.widget.TextView
 import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var edit_Frase : EditText
+    private lateinit var TvFrase: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var TvFrase: TextView
+
         TvFrase = findViewById(R.id.textViewInstruccion)
-        var edit_Frase : EditText = findViewById(R.id.editTextFrase)
+        edit_Frase  = findViewById(R.id.editTextFrase)
          var bnmostrar = findViewById<Button>(R.id.button_Mostrar)
         bnmostrar.setOnClickListener {
-            val FRASE = edit_Frase.text.toString()
+            val FRASE = getFrase()
             Snackbar.make(TvFrase, "Su Frase fue $FRASE",Snackbar.LENGTH_LONG).show()
         }
+        var bnSiguiente = findViewById<Button>(R.id.bnSiguiente)
+        bnSiguiente.setOnClickListener{
+            var i = Intent(this, SegundaActivity::class.java)
+            i.putExtra("Frase",getFrase())
+            i.putExtra("Edad",21)
+            startActivity(i)
+        }
+    }
+    private fun  getFrase():String{
+        return edit_Frase.text.toString()
     }
 }
--- app/src/main/java/com/example/botones/SegundaActivity.kt
+++ app/src/main/java/com/example/botones/SegundaActivity.kt
@@ -3,16 +3,28 @@ package com.example.botones
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 
 class SegundaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_segunda)
+
+        var fraseRecibida = intent.getStringExtra("Frase")
+        var edad = intent.getIntExtra("Edad", 0)
+
+        Log.d("Datos", "Datos rebidos con exito fueron $fraseRecibida y edad $edad")
+
         var bnAbrirPantalla = findViewById<Button>(R.id.btnPantalla)
         bnAbrirPantalla.setOnClickListener {
             val i = Intent(this, MainActivity::class.java)
             startActivity(i)
+            finish()
+        }
+        var bnCerrar = findViewById<Button>(R.id.bnCerrar)
+        bnCerrar.setOnClickListener{
+            finish()
         }
     }
 }
